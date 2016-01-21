102470f4254cf27e351b5b3900ebbcd9b4bf6f97
Robert <rwatt@four51.com>
2016-01-21 16:37:48 -0600

    Common Directory Update: buyer-select Using new sdk

--- src/app/common/buyer-select/ordercloud-buyer-selected.js
+++ src/app/common/buyer-select/ordercloud-buyer-selected.js
@@ -15,28 +15,28 @@ function SelectBuyerDirective() {
     }
 }
 
-function SelectBuyerController($state, Buyers, BuyerID) {
+function SelectBuyerController($state, OrderCloud) {
     var vm = this;
 
-    Buyers.List().then(function(data) {
+    OrderCloud.Buyers.List().then(function(data) {
         vm.BuyerList = data;
     });
 
-    Buyers.Get(BuyerID.Get()).then(function(data) {
+    OrderCloud.Buyers.Get(OrderCloud.BuyerID.Get()).then(function(data) {
         vm.selectedBuyer = data;
     });
 
     vm.ChangeBuyer = function(buyer) {
-        Buyers.Get(buyer.ID).then(function(data) {
+        OrderCloud.Buyers.Get(buyer.ID).then(function(data) {
             vm.selectedBuyer = data;
-            BuyerID.Set(data.ID);
+            OrderCloud.BuyerID.Set(data.ID);
             $state.reload($state.current);
         });
     };
 
     vm.pagingfunction = function() {
         if (vm.BuyerList.Meta.Page <= vm.BuyerList.Meta.TotalPages) {
-            Buyers.List(null, vm.BuyerList.Meta.Page + 1, vm.BuyerList.Meta.PageSize)
+            OrderCloud.Buyers.List(null, vm.BuyerList.Meta.Page + 1, vm.BuyerList.Meta.PageSize)
                 .then(function(data) {
                     vm.BuyerList.Meta = data.Meta;
                     vm.BuyerList.Items = [].concat(vm.BuyerList.Items, data.Items);
