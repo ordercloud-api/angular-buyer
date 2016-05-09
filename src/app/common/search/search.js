1ecfad1f1424987b1fee4af2054fe8448db09908
Kyle Olson <kolson@four51.com>
2016-05-09 10:37:43 -0500

    OC-403 Shipments- Add search

--- src/app/common/search/search.js
+++ src/app/common/search/search.js
@@ -51,10 +51,24 @@ function ordercloudSearchCtrl($timeout, $scope, OrderCloud, TrackSearch) {
                             });
                     }
                 }
-                if ($scope.servicename === 'SpendingAccounts') {
+                else if ($scope.servicename === 'SpendingAccounts') {
                     if (!$scope.controlleras.searchfunction) {
                         Service.List(n, null, null, null, null, {'RedemptionCode': '!*'})
                             .then(function (data){
+                                $scope.controlleras.list = data;
+                            });
+                    }
+                    else {
+                        $scope.controlleras.searchfunction($scope.searchTerm)
+                            .then(function (data){
+                                $scope.controlleras.list = data;
+                            });
+                    }
+                }
+                else if ($scope.servicename === 'Shipments') {
+                    if (!$scope.controlleras.searchfunction) {
+                        Service.List(null, n, null, null)
+                            .then(function (data) {
                                 $scope.controlleras.list = data;
                             });
                     }
