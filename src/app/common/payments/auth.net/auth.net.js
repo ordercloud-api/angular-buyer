f3777a0101782304264ded5cc8a8e114c2bc1a6f
Zeeshan Dawood <zdawood@four51.com>
2016-12-07 18:21:07 -0600

    CCN-38 Create CreditCard AuthNet service setup

--- src/app/common/payments/auth.net/auth.net.js
+++ src/app/common/payments/auth.net/auth.net.js
@@ -2,7 +2,7 @@ angular.module('ordercloud-payment-authorizeNet', [])
     .factory('AuthorizeNet', AuthorizeNet)
 ;
 
-function AuthorizeNet( $q, $resource) {
+function AuthorizeNet( $q, $resource, OrderCloud) {
     return {
         'CreateCreditCard': _createCreateCard,
         'UpdateCreditCard': _updateUpdateCreditCard(),
@@ -11,9 +11,30 @@ function AuthorizeNet( $q, $resource) {
 
     };
 
-    function _createCreateCard(creditCard) {
+    // function _update(orderID, lineItemID, lineItem, buyerID) {
+    //     return makeApiCall('PUT', '/v1/buyers/:buyerID/orders/:orderID/lineitems/:lineItemID', {
+    //         'buyerID': buyerID ? buyerID : BuyerID().Get(),
+    //         'orderID': orderID,
+    //         'lineItemID': lineItemID
+    //     }, lineItem);
+    // }
+
+    function _createCreateCard(creditCard, buyerID) {
           console.log(creditCard);
-        // makeApiCall('POST',{} )
+          var year = creditCard.ExpirationYear.toString().substring(2,4);
+            var ExpirationDate = creditCard.ExpirationMonth.concat(year);
+
+        return makeApiCall('POST',{
+            'buyerID' : buyerID ? buyerID : OrderCloud.BuyerID.Get(),
+            'TransactionType' : "createCreditCard",
+            'CardDetails' : {
+                'CardholderName' : creditCard.CardholderName,
+                'CardType' : creditCard.CardType,
+                'CardNumber' : creditCard.CardNumber,
+                'ExpirationDate' : ExpirationDate,
+                'CardCode' : creditCard.CardCode
+            }
+        });
         // return  a;
     }
 
@@ -29,17 +50,18 @@ function AuthorizeNet( $q, $resource) {
 
 
     function makeApiCall(method, requestBody) {
-        var apiUrl = 'https://api.ordercloud.io/v1/nativeintegrationproxy/authorizenet';
+        var apiUrl = 'https://api.ordercloud.io/v1/integrationproxy/authorizenettest';
         var d = $q.defer();
         $resource(apiUrl, null, {
             callApi: {
                 method: method,
                 headers: {
-                    'Authorization': 'Bearer ' + Auth().ReadToken()
+                    'Authorization': 'Bearer ' + OrderCloud.Auth.ReadToken()
                 }
             }
         }).callApi(requestBody).$promise
             .then(function(data) {
+                console.log(data);
                 d.resolve(data);
             })
             .catch(function(ex) {
