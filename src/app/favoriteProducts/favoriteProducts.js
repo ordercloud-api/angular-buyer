e55e46c328091dff7c2e6f744b0c3c50f8ecdb5a
Zeeshan Dawood <zdawood@four51.com>
2017-03-20 16:29:37 -0500

    [OC-2058] update unit test clean code as result of unit test

--- src/app/favoriteProducts/favoriteProducts.js
+++ src/app/favoriteProducts/favoriteProducts.js
@@ -109,55 +109,66 @@ function FavoriteProductDirective(){
 
 function FavoriteProductController($scope, OrderCloud, toastr){
     var vm = this;
-    if($scope.currentUser && $scope.currentUser.xp && $scope.currentUser.xp.FavoriteProducts){
-        vm.hasFavorites = true;
-    }
-    else{
-        if($scope.currentUser && $scope.currentUser.xp){
-            $scope.currentUser.xp.FavoriteProducts = [];
-        }else{
-            $scope.currentUser.xp ={};
-            $scope.currentUser.xp.FavoriteProducts = [];
-        }
-        OrderCloud.Me.Patch( {xp:$scope.currentUser.xp})
-            .then(function(){
-                vm.hasFavorites = true;
-            })
-            .catch(function(ex){
-                console.log(ex);
-            });
-    }
+    vm.checkHasFavorites = checkHasFavorites;
+    vm.toggleFavoriteProduct = toggleFavoriteProduct;
+    vm.addProduct = addProduct;
+    vm.removeProduct = removeProduct;
+
+    vm.checkHasFavorites();
     vm.isFavorited = vm.hasFavorites && $scope.currentUser.xp.FavoriteProducts.indexOf($scope.product.ID) > -1;
 
-    vm.toggleFavoriteProduct = function(){
+    function toggleFavoriteProduct(){
         if (vm.hasFavorites){
             if (vm.isFavorited){
                 removeProduct();
             } else {
                 addProduct($scope.currentUser.xp.FavoriteProducts);
             }
-
         } else {
             addProduct([]);
         }
-        function addProduct(existingList){
-            existingList.push($scope.product.ID);
-            OrderCloud.Me.Patch({xp: {FavoriteProducts: existingList}})
-                .then(function(data){
-                    vm.hasFavorites = data.xp && data.xp.FavoriteProducts;
-                    vm.isFavorited = true;
-                    toastr.success($scope.product.Name + ' was added to your favorites');
-                });
+    }
+
+    function checkHasFavorites(){
+        if($scope.currentUser && $scope.currentUser.xp && $scope.currentUser.xp.FavoriteProducts){
+            vm.hasFavorites = true;
         }
-        function removeProduct(){
-            var updatedList = _.without($scope.currentUser.xp.FavoriteProducts, $scope.product.ID);
-            OrderCloud.Me.Patch({xp: {FavoriteProducts: updatedList}})
+        else{
+            if($scope.currentUser && $scope.currentUser.xp){
+                $scope.currentUser.xp.FavoriteProducts = [];
+            }else{
+                $scope.currentUser.xp ={};
+                $scope.currentUser.xp.FavoriteProducts = [];
+            }
+            OrderCloud.Me.Patch( {xp:$scope.currentUser.xp})
                 .then(function(){
-                    vm.isFavorited = false;
-                    $scope.currentUser.xp.FavoriteProducts = updatedList;
-                    toastr.success($scope.product.Name + ' was removed from your favorites');
+                    vm.hasFavorites = true;
+                })
+                .catch(function(ex){
+                    console.log(ex);
                 });
         }
-    };
+    }
+
+    function addProduct(existingList){
+        existingList.push($scope.product.ID);
+        OrderCloud.Me.Patch({xp: {FavoriteProducts: existingList}})
+            .then(function(data){
+                vm.hasFavorites = data.xp && data.xp.FavoriteProducts;
+                vm.isFavorited = true;
+                toastr.success($scope.product.Name + ' was added to your favorites');
+            });
+    }
+
+    function removeProduct(){
+        var updatedList = _.without($scope.currentUser.xp.FavoriteProducts, $scope.product.ID);
+        OrderCloud.Me.Patch({xp: {FavoriteProducts: updatedList}})
+            .then(function(){
+                vm.isFavorited = false;
+                $scope.currentUser.xp.FavoriteProducts = updatedList;
+                toastr.success($scope.product.Name + ' was removed from your favorites');
+            });
+    }
+
 }
 
