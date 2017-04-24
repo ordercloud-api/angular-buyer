b4d191941dc55013472ea0ab0795a1b145daf28d
Kyle Olson <kolson@four51.com>
2017-04-24 17:01:35 -0500

    Remove ocInterceptors and LoginService

--- src/app/app.controller.js
+++ src/app/app.controller.js
@@ -2,13 +2,13 @@ angular.module('orderCloud')
     .controller('AppCtrl', AppController)
 ;
 
-function AppController($state, $ocMedia, LoginService, ocAppName, ocStateLoading, ocIsTouchDevice, ocRoles) {
+function AppController($state, $ocMedia, OrderCloudSDK, ocAppName, ocStateLoading, ocIsTouchDevice, ocRoles) {
     var vm = this;
     vm.name = ocAppName.Watch;
     vm.$state = $state;
     vm.$ocMedia = $ocMedia;
     vm.isTouchDevice = ocIsTouchDevice;
     vm.stateLoading = ocStateLoading.Watch;
-    vm.logout = LoginService.Logout;
+    vm.logout = OrderCloudSDK.Auth.Logout;
     vm.userIsAuthorized = ocRoles.UserIsAuthorized;
 }
