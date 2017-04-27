be745124f028c461d881123dd5a78a2cd222d2d0
Eric Sitarz <esitarz@fou51.com>
2017-04-27 16:05:24 -0500

    Add uib-carousel
    * Create 5-image carousel on landing page.
    * [WIP] Style homepage and reformat for improved responsiveness.
    
    OC-2131

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -20,4 +20,32 @@ function HomeConfig($stateProvider) {
 
 function HomeController() {
 	var vm = this;
+
+	vm.slides = [
+		{
+			id: 0,
+			image:'assets/images/Carousel_1.jpg',
+			text: 'Slide 1'
+		},
+		{
+			id: 1,
+			image:'assets/images/Carousel_2.jpg',
+			text: 'Slide 2'
+		},
+		{
+			id: 2,
+			image:'assets/images/Carousel_3.jpg',
+			text: 'Slide 3'
+		},
+		{
+			id: 3,
+			image:'assets/images/Carousel_4.jpg',
+			text: 'Slide 4'
+		},
+		{
+			id: 4,
+			image:'assets/images/Carousel_5.jpg',
+			text: 'Slide 5'
+		}
+	]
 }
