5b2e7246f972e2582ea8b22df2e98ea46b9b08a3
Eric Sitarz <esitarz@fou51.com>
2017-04-28 15:24:55 -0500

    Add uib-carousel on updated angular-buyer app
    * Update carousel controller for each home.js file.
    
    OC-2115

--- src/app/home/js/home.controller.js
+++ src/app/home/js/home.controller.js
@@ -2,6 +2,34 @@ angular.module('orderCloud')
 	.controller('HomeCtrl', HomeController)
 ;
 
-function HomeController() {
+function HomeController(appname) {
 	var vm = this;
+	vm.appname = appname;
+	vm.slides = [
+		{
+			id: 0,
+			image:'assets/images/Carousel_1.jpg',
+			text: 'Slide 1'
+		},
+		{
+			id: 1,
+			image:'assets/images/Carousel_2.jpg',
+			text: 'Slide 2'
+		},
+		{
+			id: 2,
+			image:'assets/images/Carousel_3.jpg',
+			text: 'Slide 3'
+		},
+		{
+			id: 3,
+			image:'assets/images/Carousel_4.jpg',
+			text: 'Slide 4'
+		},
+		{
+			id: 4,
+			image:'assets/images/Carousel_5.jpg',
+			text: 'Slide 5'
+		}
+	]
 }
