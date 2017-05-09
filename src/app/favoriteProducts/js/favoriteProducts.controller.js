e40f2febc3e212ab180c5f0780ff0c27dd38f329
Crhistian Ramirez <cramirez@four51.com>
2017-05-09 11:54:22 -0500

    refactor: make favoriteProducts more testable
    also update code to align with our code standards
    
    F51-122

--- src/app/favoriteProducts/js/favoriteProducts.controller.js
+++ src/app/favoriteProducts/js/favoriteProducts.controller.js
@@ -7,28 +7,30 @@ function FavoriteProductsController(ocParameters, OrderCloudSDK, $state, $ocMedi
     vm.currentUser = CurrentUser;
     vm.list = FavoriteProducts;
     vm.parameters = Parameters;
-
-    vm.sortSelection = Parameters.sortBy ? (Parameters.sortBy.indexOf('!') == 0 ? Parameters.sortBy.split('!')[1] : Parameters.sortBy) : null;
+    vm.sortSelection = Parameters.sortBy ? (Parameters.sortBy.indexOf('!') === 0 ? Parameters.sortBy.split('!')[1] : Parameters.sortBy) : null;
 
     //Filtering and Search Functionality
     //check if filters are applied
     vm.filtersApplied = vm.parameters.filters || ($ocMedia('max-width: 767px') && vm.sortSelection);
     vm.showFilters = vm.filtersApplied;
-
-
-    //reload the state with new filters
-    vm.filter = function(resetPage) {
+    vm.filter = filter; //reload the state with new filters
+    vm.clearFilters = clearFilters; //clear the relevant filters, reload the state & reset the page
+    vm.updateSort = updateSort;
+    vm.reverseSort = reverseSort;
+    vm.pageChanged = pageChanged; //reload the state with the incremented page parameter
+    vm.loadMore = loadMore; //load the next page of results with all the same parameters
+    
+    function filter(resetPage) {
         $state.go('.', ocParameters.Create(vm.parameters, resetPage));
-    };
+    }
 
-    //clear the relevant filters, reload the state & reset the page
-    vm.clearFilters = function() {
+    function clearFilters() {
         vm.parameters.filters = null;
         $ocMedia('max-width: 767px') ? vm.parameters.sortBy = null : angular.noop();
         vm.filter(true);
-    };
+    }
 
-    vm.updateSort = function(value) {
+    function updateSort(value) {
         value ? angular.noop() : value = vm.sortSelection;
         switch (vm.parameters.sortBy) {
             case value:
@@ -41,27 +43,25 @@ function FavoriteProductsController(ocParameters, OrderCloudSDK, $state, $ocMedi
                 vm.parameters.sortBy = value;
         }
         vm.filter(false);
-    };
+    }
 
-    vm.reverseSort = function() {
-        Parameters.sortBy.indexOf('!') == 0 ? vm.parameters.sortBy = Parameters.sortBy.split('!')[1] : vm.parameters.sortBy = '!' + Parameters.sortBy;
+    function reverseSort() {
+        vm.parameters.sortBy.indexOf('!') === 0 ? vm.parameters.sortBy = vm.parameters.sortBy.split('!')[1] : vm.parameters.sortBy = '!' + vm.parameters.sortBy;
         vm.filter(false);
-    };
+    }
 
-    //reload the state with the incremented page parameter
-    vm.pageChanged = function() {
+    function pageChanged() {
         $state.go('.', {
             page: vm.list.Meta.Page
         });
-    };
+    }
 
-    //load the next page of results with all the same parameters
-    vm.loadMore = function() {
+    function loadMore() {
         var parameters = angular.extend(Parameters, {page:vm.list.Meta.Page + 1});
         return OrderCloudSDK.Me.ListProducts(parameters)
             .then(function(data) {
                 vm.list.Items = vm.list.Items.concat(data.Items);
                 vm.list.Meta = data.Meta;
             });
-    };
+    }
 }
