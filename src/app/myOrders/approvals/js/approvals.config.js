37d8af02fa3a6c04eabf6e8901b1f8d686400a2a
Crhistian Ramirez <cramirez@four51.com>
2017-05-10 12:06:18 -0500

    refactor: error handling & remove unnecessary code
    users are now accessible as a sub-object
    
    F51-154

--- src/app/myOrders/approvals/js/approvals.config.js
+++ src/app/myOrders/approvals/js/approvals.config.js
@@ -13,15 +13,11 @@ function ApprovalsConfig($stateProvider){
                 pageTitle: 'Order Approvals'
             },
             resolve: {
-                OrderApprovals: function($stateParams, ocApprovals) {
-                    return ocApprovals.List($stateParams.orderid, $stateParams.buyerid, 1, 100);
+                OrderApprovals: function($stateParams, ocApprovals, buyerid) {
+                    return ocApprovals.List($stateParams.orderid, buyerid);
                 },
                 CanApprove: function(CurrentUser, $stateParams, OrderCloudSDK){
-                    var parameters = {
-                        page: 1,
-                        pageSize: 100
-                    };
-                    return OrderCloudSDK.Orders.ListEligibleApprovers('outgoing', $stateParams.orderid, parameters)
+                    return OrderCloudSDK.Orders.ListEligibleApprovers('outgoing', $stateParams.orderid, {pageSize: 100})
                         .then(function(userList){
                             var userIDs = _.pluck(userList.Items, 'ID');
                             return userIDs.indexOf(CurrentUser.ID) > -1;
