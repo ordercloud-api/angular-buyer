3647a9d06bc4dd9c12f38c986f58167d4eb3355f
Robert Watt <rwatt@four51.com>
2017-05-16 12:34:37 -0500

    F51-214 - Add anonymous auth to ocRefreshToken.js
    - When app.constants.json > anonymous is set to true, the ocRefreshToken
      service will attempt to authorize anonymously using the current clientid
      and scope constants.
    - An error will be logged to the console if the clientid is not configured
      to work with anonymous authentication.

--- src/app/common/services/oc-refresh-token/oc-refresh-token.js
+++ src/app/common/services/oc-refresh-token/oc-refresh-token.js
@@ -2,7 +2,7 @@ angular.module('orderCloud')
     .factory('ocRefreshToken', OrderCloudRefreshTokenService)
 ;
 
-function OrderCloudRefreshTokenService($rootScope, $state, toastr, OrderCloudSDK, clientid, scope, defaultstate) {
+function OrderCloudRefreshTokenService($exceptionHandler, $rootScope, $state, $log, toastr, OrderCloudSDK, clientid, scope, defaultstate, anonymous) {
     $rootScope.$on('OC:InvalidOrExpiredAccessToken', function() {
         refreshToken();
     });
@@ -13,17 +13,37 @@ function OrderCloudRefreshTokenService($rootScope, $state, toastr, OrderCloudSDK
             OrderCloudSDK.Auth.RefreshToken(token, clientid, scope)
                 .then(function(data) {
                     OrderCloudSDK.SetToken(data.access_token);
-                    if ($state.current.name === '') {
-                        $state.go(defaultstate);
-                    } else {
-                        $state.go($state.current.name, {}, {reload:true});
-                    }
+                    _redirect();
                 })
                 .catch(function () {
-                    _logout();
+                    anonymous ? _authAnonymous() : _logout();
                 });
         } else {
-            _logout();
+            anonymous ? _authAnonymous() : _logout();
+        }
+
+        function _authAnonymous() {
+            OrderCloudSDK.Auth.Anonymous(clientid, scope)
+                .then(function(data) {
+                    OrderCloudSDK.SetToken(data.access_token);
+                    if (data.refresh_token) OrderCloudSDK.SetRefreshToken(data.refresh_token);
+                    _redirect();
+                })
+                .catch(function(ex) {
+                    if (ex.response && ex.response.body && ex.response.body.error === 'invalid_grant') {
+                        $log.error('The current clientid is not configured for anonymous shopping.');
+                    } else {
+                        $exceptionHandler(ex);
+                    }
+                });
+        }
+
+        function _redirect() {
+            if ($state.current.name === '') {
+                $state.go(defaultstate);
+            } else {
+                $state.go($state.current.name, {}, {reload:true});
+            }
         }
 
         function _logout() {
