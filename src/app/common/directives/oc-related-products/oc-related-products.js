694405ca92e8c5252a02e6cb94fed78342f0082e
Alexa Rarick <ararick@four51.com>
2017-05-18 14:59:30 -0500

    F51-28 feature: related products directive

---
+++ src/app/common/directives/oc-related-products/oc-related-products.js
@@ -0,0 +1,50 @@
+angular.module('orderCloud')
+    .directive('ocRelatedProducts', RelatedProductsDirective)
+;
+
+function RelatedProductsDirective(ocRelatedProducts, $compile) {
+    return {
+        scope: {
+            product: "=",
+            currentuser: "="
+        },
+        restrict: 'E',
+        link: function(scope, element) {
+            if(scope.product && scope.product.xp && scope.product.xp.RelatedProducts){
+                ocRelatedProducts.List(scope.product.xp.RelatedProducts)
+                    .then(function(data){
+                        console.log(scope.product);
+                        scope.relatedProducts = data.Items;
+                        scope.responsive = [
+                            {
+                                breakpoint: 1500,
+                                settings: {
+                                    slidesToShow: 4
+                                }
+                            },
+                            {
+                                breakpoint: 992,
+                                settings: {
+                                    slidesToShow: 3
+                                }
+                            },
+                            {
+                                breakpoint: 768,
+                                settings: {
+                                    slidesToShow: 1
+                                }
+                            }                            
+                        ];
+                        element.html(
+                            "<slick arrows='!application.isTouchDevice' responsive='responsive' infinite='false' slides-to-show='6' slides-to-scroll='1' class='slider multiple-items' ng-class='{\"has-arrows\": !application.isTouchDevice}'>" +
+                            "<div ng-repeat='product in relatedProducts'>" +
+                            "<div class='c-related-products__card-wrap' ng-include='\"common/directives/oc-related-products/oc-related-products.html\"'></div>" +
+                            "</div>" +
+                            "</slick>"
+                        );
+                        $compile(element.contents())(scope);
+                    });
+            }
+        }
+    }
+}
