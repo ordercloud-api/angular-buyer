31792fc8ce2b982e7b6b7a7ff2833799fd561417
Alexa Rarick <ararick@four51.com>
2017-05-22 09:18:37 -0500

    F51-118 fix: work on unit test for product detail

--- src/app/productDetail/test/productView.spec.js
+++ src/app/productDetail/test/productView.spec.js
@@ -54,4 +54,34 @@ describe('Component: ProductDetail', function() {
             })
         })
     });
+
+    describe('Directive: ocSpecForm', function() {
+        var element,
+            specs;
+        beforeEach(function() {
+            var defer = q.defer();
+            defer.resolve(specs);
+            spyOn(oc.Me, 'ListSpecs').and.returnValue(defer.promise);
+            //mock.product = {ID: 'MockProductID'};
+            scope.mockProduct = mock.Product;
+            specs = [
+                {
+                    "mockSpec": [
+                        "mockOption"
+                    ]
+                }
+            ];
+            element = compile('<oc-spec-form product="mockProduct"></oc-spec-form>')(scope);
+        });
+        it('should initialize the isolate scope', function() {
+            expect(element.isolateScope().product).toEqual(mock.product);
+        })
+        it('should call Me.ListSpecs with a product ID and parameters', function() {
+            var parameters = {
+                page: 1,
+                pageSize: 100
+            };
+            expect(oc.Me.ListSpecs).toHaveBeenCalledWith(mock.Product.ID, parameters);
+        })
+    })
 });
