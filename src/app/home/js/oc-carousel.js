eb754bf2d0a113d858a53bdf46a9ea0093ff598f
Alexa Rarick <ararick@four51.com>
2017-05-23 16:18:39 -0500

    F51-94 feat: add static images to assets folder

--- src/app/home/js/oc-carousel.js
+++ src/app/home/js/oc-carousel.js
@@ -9,17 +9,17 @@ function ocCarouselDirective($compile) {
         },
         restrict: 'E',
         link: function(scope, element) {
-            if(scope.buyer.xp && scope.buyer.xp.Images && scope.buyer.xp.Images.length) {
-                scope.interval = 5000;
-                scope.noWrapSlides = false;
+            if(scope.buyer.xp && scope.buyer.xp.Images.Items && scope.buyer.xp.Images.Items.length) {
+                scope.interval = scope.buyer.xp.Images.Interval;
+                scope.noWrapSlides = scope.buyer.xp.Images.NoWrap;
                 scope.active = 0;
                 element.html(
                     "<uib-carousel active='active' interval='interval' no-wrap='noWrapSlides'>" +
-                        "<uib-slide ng-repeat='slide in buyer.xp.Images track by slide.ID' index='$index'>" +
+                        "<uib-slide ng-repeat='slide in buyer.xp.Images.Items track by slide.ID' index='$index'>" +
                             "<img class='img-responsive' ng-src='{{slide.Src}}'>" +
                             "<div class='carousel-caption'>" +
-                                "<h4>Slide {{slide.ID}}</h4>" +
-                                "<p>{{slide.Text}}</p>" +
+                                "<h4>{{slide.Title}}</h4>" +
+                                "<p>{{slide.SubText}}</p>" +
                             "</div>" +
                         "</uib-slide>" +
                     "</uib-carousel>"
