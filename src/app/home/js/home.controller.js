eef52546a596db9b4093380c2e10043fffc44e62
Alexa Rarick <ararick@four51.com>
2017-05-24 08:28:52 -0500

    F51-94 feat: static fall back images

--- src/app/home/js/home.controller.js
+++ src/app/home/js/home.controller.js
@@ -5,4 +5,43 @@ angular.module('orderCloud')
 function HomeController(Buyer) {
 	var vm = this;
 	vm.buyer = Buyer;
+
+	vm.slides = [
+		{
+			Src: 'src/assets/images/carousel1.jpg', 
+			Title: 'Carousel Image One',
+			SubText: 'This is the first image',
+			ID: 0
+		},
+		{
+			Src: 'src/assets/images/carousel2.jpg',
+			Title: 'Carousel Image Two',
+			SubText: 'This is the second image',
+			ID: 1
+		},
+		{
+			Src: 'src/assets/images/carousel3.jpg',
+			Title: 'Carousel Image Three',
+			SubText: 'This is the third image',
+			ID: 2
+		},
+		{
+			Src: 'src/assets/images/carousel4.jpg',
+			Title: 'Carousel Image Four',
+			SubText: 'This is the fourth image',
+			ID: 3
+		},
+		{
+			Src: 'src/assets/images/carousel5.jpg',
+			Title: 'Carousel Image Five',
+			SubText: 'This is the fifth image',
+			ID: 4
+		},
+		{
+			Src: 'src/assets/images/carousel6.jpg',
+			Title: 'Carousel Image Six',
+			SubText: 'This is the sixth image',
+			ID: 5
+		},
+	]
 }
--- src/app/home/js/oc-carousel.js
+++ src/app/home/js/oc-carousel.js
@@ -5,27 +5,35 @@ angular.module('orderCloud')
 function ocCarouselDirective($compile) {
     return {
         scope: {
-            buyer: '='
+            buyer: '=',
+            slides: '='
         },
         restrict: 'E',
         link: function(scope, element) {
-            if(scope.buyer.xp && scope.buyer.xp.Images.Items && scope.buyer.xp.Images.Items.length) {
-                scope.interval = scope.buyer.xp.Images.Interval;
-                scope.noWrapSlides = scope.buyer.xp.Images.NoWrap;
-                scope.active = 0;
-                element.html(
-                    "<uib-carousel active='active' interval='interval' no-wrap='noWrapSlides'>" +
-                        "<uib-slide ng-repeat='slide in buyer.xp.Images.Items track by slide.ID' index='$index'>" +
-                            "<img class='img-responsive' ng-src='{{slide.Src}}'>" +
-                            "<div class='carousel-caption'>" +
-                                "<h4>{{slide.Title}}</h4>" +
-                                "<p>{{slide.SubText}}</p>" +
-                            "</div>" +
-                        "</uib-slide>" +
-                    "</uib-carousel>"
-                );
-                $compile(element.contents())(scope);
+            var buyer = scope.buyer;
+            var slides = scope.slides;
+            var repeater;
+            if (buyer.xp && buyer.xp.Images && buyer.xp.Images.Items && buyer.xp.Images.Items.length) {
+                scope.interval = buyer.xp.Images.Interval;
+                scope.noWrapSlides = buyer.xp.Images.NoWrap;
+            } else {
+                scope.interval = 5000;
+                scope.noWrapSlides = false;
             }
+            scope.active = 0;
+            console.log(scope.slides);
+            element.html(
+                "<uib-carousel active='active' interval='interval' no-wrap='noWrapSlides'>" +
+                    "<uib-slide ng-repeat='slide in buyer.xp.Images.Items || slides track by slide.ID' index='$index'>" +
+                        "<img class='img-responsive' ng-src='{{slide.Src}}'>" +
+                        "<div class='carousel-caption'>" +
+                            "<h4>{{slide.Title}}</h4>" +
+                            "<p>{{slide.SubText}}</p>" +
+                        "</div>" +
+                    "</uib-slide>" +
+                "</uib-carousel>"
+            );
+            $compile(element.contents())(scope);
         }
     }
 }
