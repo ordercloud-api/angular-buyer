c81ebd92791087bf697453d334c16abcdb9d4380
Alexa Rarick <ararick@four51.com>
2017-05-25 14:25:00 -0500

    F51-94 fix: conditional statement if buyer data or slide data not configured

--- src/app/common/directives/oc-carousel/oc-carousel.js
+++ src/app/common/directives/oc-carousel/oc-carousel.js
@@ -10,19 +10,22 @@ function ocCarouselDirective($compile, $templateRequest, $exceptionHandler) {
         },
         restrict: 'E',
         link: function(scope, element) {
-            if(typeof scope.buyer === 'undefined'){
-                $exceptionHandler({message: 'ocCarousel directive is not configured correctly, missing buyer object data'});
-            }
-            if((!scope.buyer) && typeof scope.slides === 'undefined'){
-                $exceptionHandler({message: 'ocCarousel directive is not configured correctly, missing slides data'});
-            }
             var buyer = scope.buyer;
-            if (buyer.xp && buyer.xp.Slides && buyer.xp.Slides.Items && buyer.xp.Slides.Items.length) {
-                scope.interval = buyer.xp.Slides.Interval;
-                scope.noWrapSlides = buyer.xp.Slides.NoWrap;
+            if(!buyer) {
+                $exceptionHandler({message: 'ocCarousel directive is not configured correctly, missing buyer data'});
             } else {
-                scope.interval = 5000;
-                scope.noWrapSlides = false;
+                var slides = (buyer && buyer.xp && buyer.xp.Slides && buyer.xp.Slides.Items && buyer.xp.Slides.Items.length) ? buyer.xp.Slides : null;
+                if((!slides) && typeof scope.slides === 'undefined') {
+                    $exceptionHandler({message: 'ocCarousel directive is not configured correctly, missing slides data'});
+                } else {
+                    if (buyer.xp && buyer.xp.Slides && buyer.xp.Slides.Items && buyer.xp.Slides.Items.length) {
+                        scope.interval = buyer.xp.Slides.Interval;
+                        scope.noWrapSlides = buyer.xp.Slides.NoWrap;
+                    } else {
+                        scope.interval = 5000;
+                        scope.noWrapSlides = false;
+                    }
+                }
             }
             scope.active = 0;
             $templateRequest("common/directives/oc-carousel/oc-carousel.html").then(function(html) {
