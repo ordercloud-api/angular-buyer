53f200a0ad56b131118d2faf1a1ca1adca943464
Robert Watt <rwatt@four51.com>
2017-06-07 15:44:03 -0500

    Updates to the oc-product-carousel directive

--- src/app/common/directives/oc-product-carousel/oc-product-carousel.js
+++ src/app/common/directives/oc-product-carousel/oc-product-carousel.js
@@ -12,7 +12,11 @@ function OrderCloudProductCarouselDirective() {
         restrict: 'E',
         templateUrl: 'common/directives/oc-product-carousel/oc-product-carousel.html',
         controller: function($scope, $timeout) {
+            var slickElement, slickOptions;
             this.$onInit = function() {
+                $scope.loading = true;
+                $scope.elementID = generateElementID();
+                
                 var defaultSlidesToShow = 6;
                 var defaultResponsiveOptions = [
                     {
@@ -48,11 +52,38 @@ function OrderCloudProductCarouselDirective() {
                     responsive: defaultResponsiveOptions,
                     slidesToShow: defaultSlidesToShow
                 };
-                var slickOptions = angular.extend(defaultOptions, $scope.options || {});
-                $scope.carouselLoading = $timeout(function() {
-                    var slickElement = $('#ProductCarousel');
+
+                slickOptions = angular.extend(defaultOptions, $scope.options || {});
+
+                //Generates a random ID to be used by the .c-product-carousel element 
+                //unique IDs are needed for multiple <oc-product-carousel></oc-product-carousel> on a single page
+                function generateElementID() {
+                    var S4 = function() {
+                        return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+                    };
+                    return (S4()+'_'+S4());
+                }
+                
+                function _initializeCarousel() {
+
+                    //Store the slick element
+                    slickElement = $('#' + $scope.elementID);
+
+                    //After the slick element is initialized, set loading to false to show the directive
+                    slickElement.on('init', function(slick) {
+                        $scope.loading = false;
+                    });
+
+                    //Initiate the slick carousel with the merged slick options object
                     slickElement.slick(slickOptions);
-                }, 300);
+                }
+
+                if ($scope.products && $scope.products.Items.length) {
+                    $scope.carouselLoading = $timeout(_initializeCarousel, 300);
+                } else {
+                    //If there are no products available, do not initialize the slick carousel.
+                    $scope.loading = false;
+                }
             };
         }
     };
