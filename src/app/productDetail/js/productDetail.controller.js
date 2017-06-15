763f47d4d65b90d33e19bc6fed0538614c5da029
Alexa Rarick <ararick@four51.com>
2017-06-15 11:20:29 -0500

    Display additional images below primary product image (#134)
    * F51-386
    feat: add slick carousel template for multiple product images
    
    * F51-386
    feat: repeat over product.xp.Images
    
    * F51-386
    refactor: comment out responsive breakpoint, add infinite false to slick carousel
    
    * F51-386
    refactor: add template request to directive
    
    * F51-386
    chore: add angular-ez-plus, zoom library
    
    * F51-386
    refactor: add timeout function in directive
    
    * F51-386
    feat: product zoom using ez-plus directive
    
    * reset branch to previous commit
    
    * reset branch to previous commit
    
    * refactor: wrap template request in timeout, add center-mode attr to slick carousel nav
    
    * refactor: add modal for product images
    
    F51-386
    
    * refactor: update the slick carousel usage to a directive
    
    * refactor: move product images carousel and modal functionality into directive
    F51-386
    
    * F51-386
    
    feat: add zoom directive (wip) for products
    
    * F51-386
    refactor: update code to reflect data structure of additional images on product
    
    * F51-386
    refactor: add conditional to open zoom modal if zoom enabled on product
    
    * F51-386
    refactor: remove use of oc-zoom directive
    
    * F51-386
    refactor: remove scope injection
    
    * F51-386
    refactor: move inline style on thumbnail for product slider to less file
    
    * F51-386
    refactor: remove empty style declaration
    
    * F51-386
    refactor: remove commented out code
    
    * F51-386
    refactor: move single image to images directive, update modal to be compatible with one image
    
    * F51-386
    refactor: remove inline style
    
    * F51-386
    refactor: remove redundant style, fix size on modal config

--- src/app/productDetail/js/productDetail.controller.js
+++ src/app/productDetail/js/productDetail.controller.js
@@ -5,11 +5,15 @@ angular.module('orderCloud')
 function ProductDetailController($exceptionHandler, Product, RelatedProducts, CurrentOrder, ocLineItems, toastr) {
     var vm = this;
     vm.item = Product;
+    vm.item.xp.image && vm.item.xp.additionalImages ? vm.item.xp.additionalImages.unshift(vm.item.xp.image) : angular.noop();
     vm.finalPriceBreak = null;
     vm.relatedProducts = RelatedProducts;
+
+    vm.addToCart = addToCart;
+    vm.findPrice = findPrice;
     
-    var toastID = 0; // This is used to circumvent the global toastr config that prevents duplicate toats from opening.
-    vm.addToCart = function() {
+    var toastID = 0; // This is used to circumvent the global toastr config that prevents duplicate toastrs from opening.
+    function addToCart() {
         ocLineItems.AddItem(CurrentOrder, vm.item)
             .then(function() {
                 toastr.success(vm.item.Name + ' was added to your cart. <span class="hidden">' + vm.item.ID + toastID + '</span>', null, {allowHtml:true});
@@ -20,7 +24,7 @@ function ProductDetailController($exceptionHandler, Product, RelatedProducts, Cu
             });
     };
 
-    vm.findPrice = function(qty){
+    function findPrice(qty){
         angular.forEach(vm.item.PriceSchedule.PriceBreaks, function(priceBreak) {
             if (priceBreak.Quantity <= qty)
                 vm.finalPriceBreak = angular.copy(priceBreak);
