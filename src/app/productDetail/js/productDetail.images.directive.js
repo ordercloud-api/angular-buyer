c10d2027834ef7088aedc3a3c52fdfc513ecb8d8
Robert Watt <rwatt@four51.com>
2017-06-15 20:31:31 -0500

    Clean up for the product image display

--- src/app/productDetail/js/productDetail.images.directive.js
+++ src/app/productDetail/js/productDetail.images.directive.js
@@ -15,16 +15,16 @@ function ocProductImages() {
                 {
                     breakpoint: 768,
                     settings: {
-                        slidesToShow: 3
+                        slidesToShow: 4
                     }
                 },
                 {
                     breakpoint: 425,
                     settings: {
-                        slidesToShow: 2
+                        slidesToShow: 3
                     }
                 }
-            ]
+            ];
             var slickMainOpts = {
                 arrows: false,
                 infinite: false,
@@ -38,7 +38,7 @@ function ocProductImages() {
                 arrows: true,
                 infinite: false,
                 responsive: responsiveOpts,
-                slidesToShow: 4,
+                slidesToShow: 5,
                 slidesToScroll: 1,
                 focusOnSelect: true,
                 asNavFor: '#ImageMain'
@@ -69,9 +69,9 @@ function ocProductImages() {
                         }
                     }}).result;
                 }
-            }
+            };
         }
-    }
+    };
 }
 
 function ProductImagesModalCtrl(Model, Index, $uibModalInstance) {
